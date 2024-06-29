863dd091aaad5c7ce8e54e71b4d1772da0026f57
ForDream丶梦空 <56014859+Forgot-Dream@users.noreply.github.com>
2024-06-29 16:49:17 +0800

    feat: update building data

--- Electricity-Spider/spider.py
+++ Electricity-Spider/spider.py
@@ -12,56 +12,112 @@ import requests
 BUILDINGS = eval("""
 [
     {
-         "buildingid":"1574231830",
-         "building":"T1"
+        "buildingid": "1503975980",
+        "building": "凤凰居6号楼"
     },
     {
-         "buildingid":"1574231835",
-         "building":"T3"
+        "buildingid": "1661835273",
+        "building": "B5号楼"
     },
     {
-         "buildingid":"1503975832",
-         "building":"S1一多书院"
+        "buildingid": "1661835256",
+        "building": "B2"
     },
     {
-         "buildingid":"1503975890",
-         "building":"S2从文书院"
+        "buildingid": "1574231830",
+        "building": "T1"
     },
     {
-         "buildingid":"1503975967",
-         "building":"S5凤凰居5号楼"
+        "buildingid": "1503975832",
+        "building": "凤凰居1号楼"
     },
     {
-         "buildingid":"1503975980",
-         "building":"S6凤凰居6号楼"
+        "buildingid": "1503975832",
+        "building": "S1一多书院"
     },
     {
-         "buildingid":"1503975980",
-         "building":"凤凰居6号楼"
+        "buildingid": "1599193777",
+        "building": "S11"
     },
     {
-         "buildingid":"1503975988",
-         "building":"S7凤凰居7号楼"
+        "buildingid": "1693031698",
+        "building": "B9"
     },
     {
-         "buildingid":"1503975995",
-         "building":"S8凤凰居8号楼"
+        "buildingid": "1503976004",
+        "building": "凤凰居9号楼"
     },
     {
-         "buildingid":"1503976004",
-         "building":"S9凤凰居9号楼"
+        "buildingid": "1503975890",
+        "building": "凤凰居2号楼"
     },
     {
-         "buildingid":"1503976004",
-         "building":"凤凰居9号楼"
+        "buildingid": "1503975967",
+        "building": "S5凤凰居5号楼"
     },
     {
-         "buildingid":"1503976037",
-         "building":"S10凤凰居10号楼"
+        "buildingid": "1503976037",
+        "building": "凤凰居10号楼"
     },
     {
-         "buildingid":"1599193777",
-         "building":"S11"
+        "buildingid": "1503975890",
+        "building": "S2从文书院"
+    },
+    {
+        "buildingid": "1693031710",
+        "building": "阅海居B10楼"
+    },
+    {
+        "buildingid": "1693031698",
+        "building": "阅海居B9楼"
+    },
+    {
+        "buildingid": "1574231835",
+        "building": "T3"
+    },
+    {
+        "buildingid": "1503976004",
+        "building": "S9凤凰居9号楼"
+    },
+    {
+        "buildingid": "1503975988",
+        "building": "S7凤凰居7号楼"
+    },
+    {
+        "buildingid": "1503976037",
+        "building": "S10凤凰居10号楼"
+    },
+    {
+        "buildingid": "1503975995",
+        "building": "S8凤凰居8号楼"
+    },
+    {
+        "buildingid": "1599193777",
+        "building": "凤凰居11/13号楼"
+    },
+    {
+        "buildingid": "1574231833",
+        "building": "专家公寓2号楼"
+    },
+    {
+        "buildingid": "1503975902",
+        "building": "凤凰居3号楼"
+    },
+    {
+        "buildingid": "1693031710",
+        "building": "B10"
+    },
+    {
+        "buildingid": "1661835249",
+        "building": "B1"
+    },
+    {
+        "buildingid": "1503975950",
+        "building": "凤凰居4号楼"
+    },
+    {
+        "buildingid": "1503975980",
+        "building": "S6凤凰居6号楼"
     }
 ]
 """)
